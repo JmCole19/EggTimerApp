ec90678040f2cc24d01c24fb1241942d37ae3371
Jason Coleman <jayemcee121@gmail.com>
2020-09-13 17:13:51 -0400

    Building and initial commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var titleView: UILabel!
+    
+    @IBAction func hardnessSelection(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+         if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds left")
+            secondsRemaining -= 1
+            titleView.text = "Cooking..."
+         } else if secondsRemaining == 0 {
+            timer.invalidate()
+            titleView.text = "Done!"
+        }
+    }
+    
 }
